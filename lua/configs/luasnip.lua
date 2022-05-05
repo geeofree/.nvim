d081648ff6cae3c02c29053a8a1d4f22280549b2
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2022-05-05 21:41:32 +0800

    Fix luasnip not working

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -7,18 +7,17 @@ ls.config.set_config({
 	enable_autosnippets = true,
 })
 
-ls.snippets = {
-	javascript = {
-		ls.parser.parse_snippet("fn", "function $1($2) {\n\t$3\n}"),
-		ls.parser.parse_snippet("afn", "async function $1($2) {\n\t$3\n}"),
-		ls.parser.parse_snippet("im", "import $1 from '$2'"),
-	},
-	typescript = {
-		ls.parser.parse_snippet("fn", "function $1($2)$3 {\n\t$4\n}"),
-		ls.parser.parse_snippet("afn", "async function $1($2)$3 {\n\t$4\n}"),
-		ls.parser.parse_snippet("im", "import $1 from '$2'"),
-	},
-}
+ls.add_snippets("javascript", {
+	ls.parser.parse_snippet("fn", "function $1($2) {\n\t$3\n}"),
+	ls.parser.parse_snippet("afn", "async function $1($2) {\n\t$3\n}"),
+	ls.parser.parse_snippet("im", "import $1 from '$2'"),
+})
+
+ls.add_snippets("typescript", {
+	ls.parser.parse_snippet("fn", "function $1($2)$3 {\n\t$4\n}"),
+	ls.parser.parse_snippet("afn", "async function $1($2)$3 {\n\t$4\n}"),
+	ls.parser.parse_snippet("im", "import $1 from '$2'"),
+})
 
 vimp.inoremap("<c-h>", function ()
 	if ls.jumpable(-1) then
