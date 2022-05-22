0d2dcc7fe80d34d2caa2e20285fe103b6d859cb5
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2022-05-22 18:56:41 +0800

    Add verbose flag to stow script

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@ then
 	mkdir ~/.config/nvim
 fi
 
-stow -t ~/.config/nvim --ignore='.*\.md' --ignore='.*\.sh' --ignore='\.git' .
+stow -v -t ~/.config/nvim --ignore='.*\.md' --ignore='.*\.sh' --ignore='\.git' .
